61b1ae5b69ce53a2812200aaa0766fc4acf7b62b
Lucas Sorzio <sorziolucas4@gmail.com>
2026-03-19 23:41:32 -0300

    primer form

--- miApp/src/app/app.component.ts
+++ miApp/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./componentes/header/header.component";
-import { FooterComponent } from "./componentes/footer/footer.component";
-import { MainComponent } from './componentes/main/main.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MainComponent } from './components/main/main.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
    standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,MainComponent],
+  imports: [ HeaderComponent, FooterComponent, MainComponent,FormComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ miApp/src/app/components/form/form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from "@angular/forms"
+import { UsersService } from '../../services/users.service';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+
+export class FormComponent implements OnInit {
+  form: FormGroup
+  usuarios: any[] = []
+
+  constructor(private fb: FormBuilder, private userService: UsersService) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        console.log(data)
+        this.usuarios = data
+      },
+      error: () => console.log("Error al traer los usuarios")
+    })
+  }
+
+  enviar() {
+    if (this.form.valid) {
+      // utilizar el servicio para realizar una petición http (addUser(user))
+      this.userService.addUser(this.form.value).subscribe({
+        next: () => {
+          this.usuarios.push(this.form.value)
+          this.form.reset()
+        }
+      })
+    }
+  }
+}
