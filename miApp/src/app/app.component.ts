edd7c8c57d800e497a128248f82576fbe18eef48
Lucas Sorzio <sorziolucas4@gmail.com>
2026-03-20 17:17:41 -0300

    arreglado de formulario doble

--- miApp/src/app/app.component.ts
+++ miApp/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { MainComponent } from './components/main/main.component';
+
 
 
 @Component({
   selector: 'app-root',
    standalone: true,
-  imports: [ HeaderComponent, FooterComponent, MainComponent, RouterOutlet],
+  imports: [ HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
