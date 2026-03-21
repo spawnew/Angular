799b71862761bfa1cdd371749d71137aa3f6aea0
Lucas Sorzio <sorziolucas4@gmail.com>
2026-03-20 22:47:43 -0300

    arreglo para que se vea mejor en pantallas chicas

--- miApp/src/app/components/main/main.component.ts
+++ miApp/src/app/components/main/main.component.ts
@@ -1,30 +1,33 @@
-  import { Component, inject } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+;
 import { CommonModule } from '@angular/common'; 
 import { ChatService } from '../../services/chat.service';
-import { FooterComponent } from '../footer/footer.component';
+import { Component, inject, computed } from '@angular/core'; // Agregá computed
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router'; // Agregá Router
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule,FooterComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
   
 export class MainComponent {
-  // Inyectamos el servicio
   chatService = inject(ChatService);
-  
-  // Lista de chats del servicio (usamos el get() de la signal)
+  router = inject(Router); // Inyectamos el router
+
   chats = this.chatService.chats;
-  
-  // ID seleccionado para el responsive
   idSeleccionado = this.chatService.idSeleccionado;
 
-  // Función para cerrar el chat en móvil
+  // Nueva Signal que detecta si debe mostrarse el contenido principal
+  mostrarContenido = computed(() => {
+    return this.idSeleccionado() !== null || this.router.url.includes('/nuevo');
+  });
+
   deseleccionarChat() {
     this.chatService.seleccionarChat(null);
+    this.router.navigate(['/chats']); // Volvemos a la lista base
   }
 }
   
